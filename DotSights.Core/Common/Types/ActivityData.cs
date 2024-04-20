f21ddeededff8f493d0df103b2f96b20c9462dca
Thiago Menezes <thiagomvas@gmail.com>
2024-04-20 14:42:18 -0300

    Add project files.

---
+++ DotSights.Core/Common/Types/ActivityData.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace DotSights.Core.Common.Types
+{
+	public class ActivityData
+	{
+		public string WindowTitle { get; set; }
+		public long FocusedTimeInSeconds { get; set; }
+		public Dictionary<DayOfWeek, int> UsageTimePerWeekDay { get; set; }
+		public Dictionary<int, int> UsageTimePerHour { get; set; }
+		public Dictionary<int, int> UsageTimePerMonth { get; set; }
+		public string Alias { get; set; }
+
+		public ActivityData(string windowTitle)
+		{
+			WindowTitle = windowTitle;
+			FocusedTimeInSeconds = 0;
+			UsageTimePerWeekDay = new Dictionary<DayOfWeek, int>();
+			UsageTimePerHour = new Dictionary<int, int>();
+			UsageTimePerMonth = new Dictionary<int, int>();
+		}
+
+		public ActivityData()
+		{
+			WindowTitle = string.Empty;
+			FocusedTimeInSeconds = 0;
+			UsageTimePerWeekDay = new Dictionary<DayOfWeek, int>();
+			UsageTimePerHour = new Dictionary<int, int>();
+			UsageTimePerMonth = new Dictionary<int, int>();
+		}
+		public static ActivityData operator ++(ActivityData activityData)
+		{
+			activityData.FocusedTimeInSeconds++;
+
+			DayOfWeek currentDay = DateTime.Now.DayOfWeek;
+			if (activityData.UsageTimePerWeekDay.ContainsKey(currentDay))
+			{
+				activityData.UsageTimePerWeekDay[currentDay]++;
+			}
+			else
+			{
+				activityData.UsageTimePerWeekDay.Add(currentDay, 1);
+			}
+
+			int currentHour = DateTime.Now.Hour;
+			if (activityData.UsageTimePerHour.ContainsKey(currentHour))
+			{
+				activityData.UsageTimePerHour[currentHour]++;
+			}
+			else
+			{
+				activityData.UsageTimePerHour.Add(currentHour, 1);
+			}
+
+			int currentMonth = DateTime.Now.Month;
+			if (activityData.UsageTimePerMonth.ContainsKey(currentMonth))
+			{
+				activityData.UsageTimePerMonth[currentMonth]++;
+			}
+			else
+			{
+				activityData.UsageTimePerMonth.Add(currentMonth, 1);
+			}
+			return activityData;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Window Title: {WindowTitle}");
+			sb.AppendLine($"Focused Time: {FocusedTimeInSeconds} seconds");
+			sb.AppendLine("Usage Time Per Week Day:");
+			foreach (var kvp in UsageTimePerWeekDay)
+			{
+				sb.AppendLine($"{kvp.Key}: {kvp.Value}");
+			}
+			sb.AppendLine("Usage Time Per Hour:");
+			foreach (var kvp in UsageTimePerHour)
+			{
+				sb.AppendLine($"{kvp.Key}: {kvp.Value}");
+			}
+			sb.AppendLine("Usage Time Per Month:");
+			foreach (var kvp in UsageTimePerMonth)
+			{
+				sb.AppendLine($"{kvp.Key}: {kvp.Value}");
+			}
+			return sb.ToString();
+		}
+	}
+}
---
+++ DotSights.Core/DotSights.cs
@@ -0,0 +1,49 @@
+﻿using DotSights.Core.Common.Types;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+
+namespace DotSights.Core;
+
+public static class DotSights
+{
+	[DllImport("user32.dll")]
+	private static extern IntPtr GetForegroundWindow();
+
+	[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+	private static extern int GetWindowText(IntPtr hWnd, string lpString, int nMaxCount);
+
+	public static string GetFocusedWindow()
+	{
+		IntPtr foregroundWindowHandle = GetForegroundWindow();
+		if (foregroundWindowHandle != IntPtr.Zero)
+		{
+			const int nChars = 256;
+			string windowTitle = new string(' ', nChars);
+			GetWindowText(foregroundWindowHandle, windowTitle, nChars);
+			return windowTitle.Substring(0, windowTitle.IndexOf('\0'));
+		}
+		return "";
+	}
+
+	public static string SerializeActivityData(List<ActivityData> data)
+	{
+		return JsonConvert.SerializeObject(data, Formatting.Indented);
+	}
+
+	public static string SerializeActivityData(ActivityData activityData)
+	{
+		return JsonConvert.SerializeObject(activityData, Formatting.Indented);
+	}
+
+	public static ActivityData? DeserializeActivityData(string serializedData)
+	{
+		return JsonConvert.DeserializeObject<ActivityData>(serializedData);
+	}
+
+	public static List<ActivityData>? DeserializeActivityDataList(string serializedData)
+	{
+		return JsonConvert.DeserializeObject<List<ActivityData>>(serializedData);
+	}
+
+}
+
